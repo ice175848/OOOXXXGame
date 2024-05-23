3190a7781f97329fda47f118e1ed4b55b11875c4
ice175848 <memory509609@gmail.com>
2024-05-23 11:12:03 +0800

    Ｖ擺放正確位置 Ｖ判斷誰贏 Ｘ遊戲結束時重來一局 Ｘ棋子>3時 第一個消失

--- OOOXXX/GameScene.swift
+++ OOOXXX/GameScene.swift
@@ -1,109 +1,190 @@
-//
-//  GameScene.swift
-//  OOOXXX
-//
-//  Created by 艾斯 on 2024/4/19.
-//
-
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+    // 定義棋盤節點
+    var boardNode: SKSpriteNode!
+    var numberOfTouches = 0
+    var helloLabel: SKLabelNode!
+    var background: SKSpriteNode!
+    var column=0
+    var row=0
+    var oCount=0
+    var xCount=0
+    var gameIsOver = false
+    var oPositions: [[Bool]] = Array(repeating: Array(repeating: false, count: 3), count: 3)
+    var xPositions: [[Bool]] = Array(repeating: Array(repeating: false, count: 3), count: 3)
 
-        self.lastUpdateTime = 0
+    let boardWidth: CGFloat = 720 // 棋盤寬度
+    let boardHeight: CGFloat = 724 // 棋盤高度
+    let numberOfRows = 3 // 棋盤行數
+    let numberOfColumns = 3 // 棋盤列數
+    
+
+
+    override func didMove(to view: SKView) {
+        // 設置背景
+        setupGameBoard()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        if gameIsOver
+        {
+            let newScene = GameScene(size: self.size)
+            newScene.scaleMode = .fill
+            view?.presentScene(newScene,transition:SKTransition.flipHorizontal(withDuration: 0.5))
+            resetGameState()
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            return
+        }
+        let location = touch.location(in: boardNode)  // 計算觸摸位置相對於 boardNode
+
+        print("Touch location in boardNode: \(location)")
+        if 0 <= location.x && location.x < boardNode.size.width && 0 <= location.y && location.y < boardNode.size.height {
+            let column = Int(location.x / 222)
+            let row = Int(location.y / 222)
+            let centerX = CGFloat(column) * 222 + 111
+            let centerY = CGFloat(row) * 222 + 111
+            if(oPositions[row][column] == true||xPositions[row][column] == true)
+            {//如果那格已經有棋子了
+                return
+            }
+
+            let imageName = (numberOfTouches % 2 == 0) ? "X.png" : "O.png"
+            //用第幾次碰觸來判斷是Ｘ回合還是Ｏ回合
+            let sprite = SKSpriteNode(imageNamed: imageName)
+            sprite.position = CGPoint(x: centerX, y: centerY)
+            sprite.zPosition = CGFloat(numberOfTouches)
+            boardNode.addChild(sprite)//放棋子
+            if(numberOfTouches%2 == 0)
+            {
+                xPositions[row][column] = true
+            }
+            else
+            {
+                oPositions[row][column] = true
+            }
+            numberOfTouches += 1
+        }
+        if checkForWin(xPositions)||checkForWin(oPositions) {
+            print("\(((numberOfTouches % 2) == 0) ? "O" : "X") wins!")
+            self.isPaused = true;
+            endGame(winner: numberOfTouches%2 == 0 ?"O":"X")
+            
+            // 可以在这里执行一些游戏结束的逻辑，比如显示胜利信息，禁止进一步触摸等
         }
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    func checkForWin(_ positions: [[Bool]]) -> Bool {
+        // 检查行
+        for row in positions {
+            if row.allSatisfy({ $0 == true }) {
+                print("game over橫線")
+                return true
+            }
         }
+
+        // 检查列
+        for col in 0..<3 {
+            if positions[0][col] && positions[1][col] && positions[2][col] {
+                print("game over直線")
+
+                return true
+
+            }
+        }
+
+        // 检查对角线
+        if (positions[0][0] && positions[1][1] && positions[2][2]) || (positions[0][2] && positions[1][1] && positions[2][0]) {
+            print("game over斜線")
+            return true
+        }
+
+        return false
+    }
+
+    func endGame(winner: String) {
+        //gameIsOver = true  // 停止接收触摸事件
+        self.isPaused = true  // 暂停场景
+        //stopTimer()  // 停止所有计时器
+        gameIsOver = true
+
+        // 显示胜利者信息
+        let winLabel = SKLabelNode(text: "\(winner) wins!")
+        winLabel.fontColor = SKColor.red
+        
+        winLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        winLabel.fontSize = 40
+        winLabel.zPosition = CGFloat(numberOfTouches+1)
+        addChild(winLabel)
+    }
+    func resetGameState() {
+        // 清除棋子
+        self.removeAllChildren()
+
+        // 重置游戏相关的变量
+        numberOfTouches = 0
+        gameIsOver = false
+        self.isPaused = false
+
+        // 重新设置棋盘
+        setupGameBoard()
+    }
+
+
+    func checkForWin(_ positions: [CGPoint]) -> Bool {
+        // 實現勝利條件判斷的邏輯（需要詳細規劃）
+        return false
+    }
+    func setupGameBoard()
+        {
+            background = SKSpriteNode(imageNamed: "OOXXbg")
+            background.position = CGPoint(x: 0, y: 0)
+            background.zPosition = -1 // 確保背景在最底層
+            addChild(background)
+            // 創建棋盤節點
+            let boardSize = CGSize(width: 666, height: 666) // 棋盤大小
+            boardNode = SKSpriteNode(color: .clear, size: boardSize)
+            boardNode.anchorPoint = CGPoint(x:0,y:0)
+            
+            boardNode.position = CGPoint(x: -333, y: -333)  // 调整Y坐标以适应视图顶部
+            addChild(boardNode)
+            // 確保遊戲場景可以接收觸摸事件
+            isUserInteractionEnabled = true
+            /*print("View size: \(view.frame.size)")
+            print("Board position: \(boardNode.position)")
+            print("Board size: \(boardNode.size)")*/
+        }
+
+    func touchDown(atPoint pos : CGPoint) {
+        // 當觸摸按下時觸發
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        // 當觸摸移動時觸發
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        // 當觸摸抬起時觸發
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        // 當觸摸移動時觸發
+        /*
+        guard let touch = touches.first else { return }
+            
+            // 獲取觸摸點的位置
+            let location = touch.location(in: self)
+            
+        print("Touch moved to \(location)")*/
+
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        // 當觸摸結束時觸發
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        // 當觸摸取消時觸發
     }
 }
--- OOOXXX/GameViewController.swift
+++ OOOXXX/GameViewController.swift
@@ -1,10 +1,3 @@
-//
-//  GameViewController.swift
-//  OOOXXX
-//
-//  Created by 艾斯 on 2024/4/19.
-//
-
 import UIKit
 import SpriteKit
 import GameplayKit
@@ -14,28 +7,14 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
             if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
                 
-                // Present the scene
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
                     
                     view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
                 }
             }
         }
